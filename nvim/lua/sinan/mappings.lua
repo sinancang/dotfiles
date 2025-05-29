3f238418c446f97b54e43399ea2a304ce440cde0
sinancang <sinancangulan@gmail.com>
2025-05-28 22:24:11 -0400

    Implement ctrl + arrow movement for insert mode as well

--- nvim/lua/sinan/mappings.lua
+++ nvim/lua/sinan/mappings.lua
@@ -6,6 +6,8 @@ local map = vim.keymap.set
 -- Configure Ctrl + Right/Left to jump over words
 map("", "<C-Right>", "e", { noremap = true })
 map("", "<C-Left>", "b", { noremap = true })
+map("i", "<C-Right>", "<Esc>eli", { noremap = true })
+map("i", "<C-Left>", "<C-O>b", { noremap = true })
 
 -- Use `jj` to exit insert mode
 map("i", "jj", "<Esc>", { noremap = true, silent = true })
@@ -16,6 +18,6 @@ map("v", "<Tab>", ">gv", { noremap = true, silent = true })
 -- Shift-Tab to indent left in visual mode
 map("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 
--- Open floating diagnostics
-vim.keymap.set("n", "+", "k", { noremap = true, silent = true })
+-- use + to move up as well
+map("n", "+", "k", { noremap = true, silent = true })
 
